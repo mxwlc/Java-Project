e7dda062672f4b9dfb68a6556eea0b91d11902bd
mxwlc <94302084+mxwlc@users.noreply.github.com>
2022-09-12 18:20:24 +0100

    Added gui

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-## Java Project
+# Java Project
 
 My aim for this java projcet is to create a simple interface that lets you store the details of a car in an sql database.
--- src/App.java
+++ src/App.java
@@ -1,10 +1,16 @@
 import java.sql.*;
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.Border;
+
+import org.w3c.dom.Text;
 
 public class App {
     static final String url = "jdbc:mysql://localhost:3306/vehicle?autoCorrect=true";
     static final String user = "root";
     static final String password = "root";
     static int maxID;
+    static final int pad = 10;
 
     static String addRow(vehicle v, int id) {
         // "insert into cars values (id, 'make', 'model', engineCapacity,
@@ -39,18 +45,94 @@ public class App {
 
     }
 
+    static void UpdateSql(String ma, String mo, double ec, int ye, double pr, Statement curr) {
+        try {
+            vehicle tempV = new vehicle(ma, mo, ec, ye, pr);
+            int maxID = findMaxID(curr) + 1;
+            String sql = addRow(tempV, maxID);
+            
+            curr.executeUpdate(sql);
+        } catch (Exception e) {
+            throw new Error("Problem", e);
+        }
+
+    }
+
     public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame();
+        ImageIcon carIcon = new ImageIcon("src/car-icon-in-flat-style-simple-traffic-icon-free-vector.webp");
+
+        JPanel border = new JPanel();
+        Border padding = BorderFactory.createEmptyBorder(pad, pad, pad, pad);
+        border.setBorder(padding);
+
+        frame.setIconImage(carIcon.getImage());
+        frame.setSize(600, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(border);
+
+        JLabel title = new JLabel("Car Database");
+        title.setFont(new Font("sansserif", Font.PLAIN, 24));
+        frame.getContentPane().add(title);
+        title.setHorizontalAlignment(JLabel.CENTER);
+
+        JLabel blank = new JLabel("");
+        frame.getContentPane().add(blank);
+
+        JLabel makeLabel = new JLabel("Make:");
+        frame.getContentPane().add(makeLabel);
+
+        JTextField inputMake = new JTextField();
+        frame.getContentPane().add(inputMake);
+
+        JLabel modelLabel = new JLabel("Model: ");
+        frame.getContentPane().add(modelLabel);
+
+        JTextField inputModel = new JTextField();
+        frame.getContentPane().add(inputModel);
+
+        JLabel EngineCapacityLabel = new JLabel("Engine Capacity: ");
+        frame.getContentPane().add(EngineCapacityLabel);
+
+        JTextField inputEngineCapacity = new JTextField();
+        frame.getContentPane().add(inputEngineCapacity);
+
+        JLabel YearLabel = new JLabel("Year Of Manufactoring: ");
+        frame.getContentPane().add(YearLabel);
+
+        JTextField inputYear = new JTextField();
+        frame.getContentPane().add(inputYear);
+
+        JLabel PriceLabel = new JLabel("Price: ");
+        frame.getContentPane().add(PriceLabel);
+
+        JTextField inputPrice = new JTextField();
+        frame.getContentPane().add(inputPrice);
+
+        frame.setLayout(new GridLayout(10, 2));
+        frame.setVisible(true);
+        JButton submit = new JButton("Submit");
+        frame.getContentPane().add(submit);
 
         try {
             Connection conn = DriverManager.getConnection(url, user, password);
 
             Statement current = conn.createStatement();
             try {
-                maxID = findMaxID(current);
+                // String input_make, input_model;
+                // double input_Engine_Capacity, input_Price;
+                // int input_Year;
 
-                vehicle v = new vehicle("vauxhall", "Corsa", 3.5, 2004, 3500.99);
-                String sql = addRow(v, maxID + 1);
-                current.executeUpdate(sql);
+                submit.addActionListener(e -> UpdateSql(inputMake.getText(), inputModel.getText(),
+                        Double.parseDouble(inputEngineCapacity.getText()), Integer.parseInt(inputYear.getText()), Double.parseDouble(inputPrice.getText()),
+                        current));
+
+                ResultSet rs = current.executeQuery("select max(id) as maxID from cars");
+                if(rs!=null){
+                    while(rs.next()){
+                        System.out.print(rs.getString("maxID"));
+                    }
+                }
 
             } catch (SQLException e) {
                 throw new Error("Problem", e);
